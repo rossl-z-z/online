e04abea0cf5a911a9525ff489cba41358125fdf9
RossL <ross.laird@gmail.com>
2017-09-30 00:45:33 -0700

    -

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 import os
-os.system("jre-9/bin/java -jar ditaa.jar ditaa_test.txt")
+os.system("./jre-9/bin/java -jar ditaa.jar ditaa_test.txt")
 
 setup (
     name             = "testapp",
