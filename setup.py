19f81801fd39cbc9ac31d59400e1bbdc96cd4725
RossL <ross.laird@gmail.com>
2017-09-30 01:00:06 -0700

    mako and pypugjs testapp changed to wsgi example

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 import os
-os.system("pwd")
+os.system("opt/app-root/src/jre-9/bin/java -jar ditaa.jar ditaa_test.txt")
 
 setup (
     name             = "testapp",
     version          = "0.1",
     description      = "Example application to be deployed.",
     packages         = find_packages(),
-    install_requires = ["gunicorn", "libsass >= 0.13"],
+    install_requires = ["gunicorn", "libsass >= 0.13", "Mako", "pypugjs"],
 )
