c88dfa994a7669206528510c8d728ee45d583948
RossL <ross.laird@gmail.com>
2017-09-30 00:43:04 -0700

    -

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from setuptools import setup, find_packages
 
 import os
-os.system("yum install java-1.8.0-openjdk")
-os.system("java -jar ditaa.jar ditaa_test.txt")
+os.system("jre-9/bin/java -jar ditaa.jar ditaa_test.txt")
 
 setup (
     name             = "testapp",
